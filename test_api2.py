2a64004a9030158ef82fde2cf6ce9dfbabd86758
Nick A. Del Grosso <delgrosso.nick@gmail.com>
2023-07-13 18:20:46 +0200

    cleaned up test

--- test_api2.py
+++ test_api2.py
@@ -5,18 +5,13 @@ from api import get_attendance_report
 def test_users_changing_display_name_doesnt_affect_attendance_duration():
     # Given the ID for a zoom meeting with where Nick <dg@gmail.com> was present for 10 minutes, 
     # # then left, and NickDG <dg@gmail.com> was present for 20 minutes afterwards
-    
-    # When you ask for the attendance report
-    # with patch('api')
-    # api.get_attendance_report
-    # assert False
-    
     def part_report(access_token, meeting_id):
         return {'participants': [
             {'status': 'in_meeting', 'name': 'Nick', 'duration': 10, 'user_email': 'dg@gmail.com'}, 
             {'status': 'in_meeting', 'name': 'NickDG', 'duration': 20, 'user_email': 'dg@gmail.com'}, 
         ]}
-        
+    
+    # When you ask for the attendance report
     with patch('api.zoom_integration.get_participant_report', part_report):
         report = get_attendance_report(token='1231414', meeting_id='1231')
     
