ffd481194003ea5b9876a18859508898e8651de1
Nick A. Del Grosso <delgrosso.nick@gmail.com>
2023-07-25 17:55:04 +0200

    added full workshop workflow

--- course_attendance_service/workflows/get_workshops.py
+++ course_attendance_service/workflows/get_workshops.py
@@ -24,6 +24,8 @@ def test_list_all_workshops_ids():
     assert workshop_ids == {given_workshops[0]['id'], given_workshops[1]['id']}
     
     
+def test_get_workshop_and_session_details():
+    random.seed(42)
     
 def test_get_workshop_details():
     random.seed(42)
@@ -35,19 +37,26 @@ def test_get_workshop_details():
             'description': rand_letters(),
             'planned_start': (s := rand_date()),
             'planned_end': (s + timedelta(days=random.randint(1, 6))),
-            'session_ids': [rand_letters(), rand_letters(), rand_letters()],
-            
+            'sessions': [
+                {'id': (sid := rand_letters()), 
+                 'planned_start': (s := rand_date()), 
+                 'planned_end': s + timedelta(hours=random.randint(3, 10)),
+                }],
+            'session_ids': [sid],
         },
     ]
     repo = InMemoryWorkshopRepo(workshops=given_workshops)
     workflows = PlannedWorkshopWorkflows(workshop_repo=repo)
     
-    workshop = workflows.get_planned_workshop_details(workshop_id=given_workshops[0]['id'])
+    workshop = workflows.get_planned_workshop_and_session_details(workshop_id=given_workshops[0]['id'])
     assert workshop.name == given_workshops[0]['name']
     assert workshop.description == given_workshops[0]['description']
     assert workshop.planned_start == given_workshops[0]['planned_start']
     assert workshop.planned_end == given_workshops[0]['planned_end']
-    assert workshop.session_ids == given_workshops[0]['session_ids']
+    given_sessions = given_workshops[0]['sessions']
+    session = workflows.get_planned_session_details(session_id=given_sessions[0]['id'])
+    assert session.start == given_sessions[0]['planned_start']
+    assert session.end == given_sessions[0]['planned_end']
     
     
 def test_get_session_details():
@@ -94,6 +103,14 @@ class PlannedSession(NamedTuple):
     start: datetime
     end: datetime
     
+class PlannedWorkshopAndSessions(NamedTuple):
+    id: WorkshopId
+    name: str
+    description: str
+    planned_start: datetime
+    planned_end: datetime
+    sessions: List[PlannedSession]
+    
 
 class WorkshopRepo(ABC):
     
@@ -122,7 +139,21 @@ class PlannedWorkshopWorkflows(NamedTuple):
         session = self.workshop_repo.get_session_details(session_id=session_id)
         return session
     
-    
+    def get_planned_workshop_and_session_details(self, workshop_id: str) -> PlannedWorkshopAndSessions:
+        workshop = self.get_planned_workshop_details(workshop_id=workshop_id)
+        sessions = []
+        for session_id in workshop.session_ids:
+            session = self.get_planned_session_details(session_id=session_id)
+            sessions.append(session)
+        workshop_full = PlannedWorkshopAndSessions(
+            id=workshop.id,
+            name=workshop.name,
+            description=workshop.description,
+            planned_start=workshop.planned_start,
+            planned_end=workshop.planned_end,
+            sessions=sessions,
+        )
+        return workshop_full
     
 class InMemoryWorkshopRepo(WorkshopRepo):
     
