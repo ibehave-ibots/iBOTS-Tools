9f6cf21e23d0affbb0b5d0bcc23e28282e35bcc9
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-07-25 16:16:55 +0000

    Add initial setup for registration workflows

---
+++ course_attendance_service/course_attendance_service/workflows/registration.py
@@ -0,0 +1,83 @@
+"""
+Make a RegistrationWorkflows class that contains methods calling an abstract RegistrantRepo interface, 
+which has a ZoomRegistrantRepo implementation. 
+Have automated tests in place for each workflow method and each ZoomRegistrantRepo method, 
+that doesn't call the Zoom API (i.e. goal is to have fast tests)
+"""
+
+
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, NamedTuple, Union
+
+###### Tests
+
+
+def test_total_number_of_registrants():
+    # GIVEN: a workshop
+    workshops = {
+        "workshop1": [Registrant(), Registrant(), Registrant()],
+        "workshop2": [Registrant(), Registrant()],
+    }
+    registrants_repo = InMemoryRegistrantsRepo(workshops)
+    registration_workflows = RegistrationWorkflows(registrants_repo)
+
+    # WHEN: asked for the number registrants
+    registrants_report1 = registration_workflows.get_registrants_report(
+        workshop_id="workshop1"
+    )
+    registrants_report2 = registration_workflows.get_registrants_report(
+        workshop_id="workshop2"
+    )
+
+    # THEN: correct number of registrants is returned
+    assert registrants_report1.total_registrants == 3
+    assert registrants_report2.total_registrants == 2
+
+
+###### Entities
+
+
+class Registrant(NamedTuple):
+    pass
+    # name: str
+    # affiliation: Union[str, List[str]]
+    # status: str
+
+
+###### Workflows
+
+
+class RegistrantsRepo(ABC):
+    @abstractmethod
+    def get_list_of_registrants(self, workshop_id: Any) -> List[Registrant]:
+        return 1
+
+
+class RegistrantsReport(NamedTuple):
+    registrants: List[Registrant]
+
+    @property
+    def total_registrants(self):
+        return len(self.registrants)
+
+
+class RegistrationWorkflows:
+    def __init__(self, registrants_repo: RegistrantsRepo):
+        self.registrants_repo = registrants_repo
+
+    def get_registrants_report(self, workshop_id):
+        registrants = self.registrants_repo.get_list_of_registrants(
+            workshop_id=workshop_id
+        )
+        return RegistrantsReport(registrants)
+
+
+###### Repositories
+
+
+class InMemoryRegistrantsRepo(RegistrantsRepo):
+    def __init__(self, workshops: Dict[Any, List[Registrant]]):
+        self.workshops = workshops
+
+    def get_list_of_registrants(self, workshop_id: Any) -> List[Registrant]:
+        return self.workshops[workshop_id]
