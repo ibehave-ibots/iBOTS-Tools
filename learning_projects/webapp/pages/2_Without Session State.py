8091bbe5907bb2b0455478b104a187b372f22562
Sangeetha <an.sangeetha@gmail.com>
2023-07-25 13:55:37 +0530

    removed unnecessary comments

--- learning_projects/webapp/pages/2_Without Session State.py
+++ learning_projects/webapp/pages/2_Without Session State.py
@@ -7,7 +7,6 @@ import seaborn as sns
 import pandas as pd
 
 def load_data():
-    # Load the Iris dataset
     iris = sns.load_dataset('iris')
     return iris
 
@@ -18,14 +17,12 @@ def without_using_session_state():
 
     col1, col2 = st.columns(2,gap='large')
 
-    # Select features to plot
     with col1:
         st.subheader("Select Features to Plot")
         available_features = data.columns[:-1]
         selected_features = st.multiselect("Select features:", available_features)
 
     with col2:
-        # Plot the data
         if selected_features:
             st.subheader("Scatter Plot")
             sns_plot = sns.pairplot(data=data, hue='species', vars=selected_features)
--- learning_projects/webapp/pages/3_With Session State.py
+++ learning_projects/webapp/pages/3_With Session State.py
@@ -7,7 +7,6 @@ import seaborn as sns
 import pandas as pd
 
 def load_data():
-    # Load the Iris dataset
     iris = sns.load_dataset('iris')
     return iris
 
@@ -22,17 +21,14 @@ def using_session_state():
 
     col1, col2 = st.columns(2,gap='large')
 
-    # Select features to plot
     with col1:
         st.subheader("Select Features to Plot")
         available_features = data.columns[:-1]
         selected_features = st.multiselect("Select features:", available_features, default=st.session_state.selected_features,key='manage')
 
-        # Update session state with selected features
         st.session_state.selected_features = selected_features
 
     with col2:
-        # Plot the data
         if selected_features:
             st.subheader("Scatter Plot")
             sns_plot = sns.pairplot(data=data, hue='species', vars=selected_features)
