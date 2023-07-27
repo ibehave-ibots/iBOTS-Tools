5967ef35aaaf5386a4d3b8449841953cec07acb7
Sangeetha <an.sangeetha@gmail.com>
2023-07-27 19:27:12 +0530

    ZoomAttendeeRepo, Unit testing of ZoomAttendeeRepo, refactored InMemoryMethods and tests

--- course_attendance_service/course_attendance_service/workflows/attendance_workflows.py
+++ course_attendance_service/course_attendance_service/workflows/attendance_workflows.py
@@ -7,68 +7,75 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, NamedTuple, Union
 import numpy as np
 import requests
-from unittest.mock import patch
+from unittest.mock import Mock
 
 
+# ZoomRepo testing
+def test_zoom_length_attendees():
+    attendees = [Attendee(name='abc',email='abc@ibehave.com',attendance=True),Attendee(name='def',email='def@gmail.com',attendance=True),Attendee(name='ghi',email='ghi@xyz.com',attendance=False)]
+    part_report = {'participants': [
+            {'status': 'in_meeting', 'name': 'abc',
+                'duration': 120, 'user_email': 'abc@ibehave.com'},
+            {'status': 'in_meeting', 'name': 'def',
+                'duration': 100, 'user_email': 'def@gmail.com'},
+            {'status': 'in_meeting', 'name': 'ghi',
+                'duration': 10, 'user_email': 'ghi@gmail.com'}                
+        ]}
 
-# InMemory testing
-def test_length_attendees():
-    attendees = [Attendee(name='abc',email='abc@ibehave.com',certificate=True),Attendee(name='def',email='def@gmail.com',certificate=True),Attendee(name='ghi',email='ghi@xyz.com',certificate=False)]
-    workshop = {'wid':attendees}
-
-    repo = InMemoryAttandeeRepo(workshop=workshop)
+    api = Mock(ZoomAttendanceApi)
+    api.get_zoom_participant_report.return_value = part_report
+    repo = ZoomAttendeeRepo(zoom_api=api)
     attendance_workflows = AttendanceWorkflows(repo)
 
-    attendance_report = attendance_workflows.list_all_attendees(workshop_id='wid')
+    attendance_report = attendance_workflows.list_all_attendees(workshop_id=1234)
+
     assert attendance_report.total_attendees == len(attendees)
 
-def test_number_of_certificates_awarded_is_right() -> None:
-    attendees = [Attendee(name='abc',email='abc@ibehave.com',certificate=True),Attendee(name='def',email='def@gmail.com',certificate=True),Attendee(name='ghi',email='ghi@xyz.com',certificate=False)]
-    workshop = {'wid':attendees}
-    repo = InMemoryAttandeeRepo(workshop=workshop)
+def test_zoom_number_of_candidates_who_got_attendance() -> None:
+    attendees = [Attendee(name='abc',email='abc@ibehave.com',attendance=True),Attendee(name='def',email='def@gmail.com',attendance=True),Attendee(name='ghi',email='ghi@xyz.com',attendance=False)]
+    part_report = {'participants': [
+            {'status': 'in_meeting', 'name': 'abc',
+                'duration': 120, 'user_email': 'abc@ibehave.com'},
+            {'status': 'in_meeting', 'name': 'def',
+                'duration': 100, 'user_email': 'def@gmail.com'},
+            {'status': 'in_meeting', 'name': 'ghi',
+                'duration': 10, 'user_email': 'ghi@gmail.com'}                
+        ]}
+
+    api = Mock(ZoomAttendanceApi)
+    api.get_zoom_participant_report.return_value = part_report
+    repo = ZoomAttendeeRepo(zoom_api=api)
     attendance_workflows = AttendanceWorkflows(repo)
 
-    attendance_report = attendance_workflows.list_all_attendees(workshop_id='wid')
-
+    attendance_report = attendance_workflows.list_all_attendees(workshop_id=1234)
     assert len(attendance_report.list_successful_attendees) == 2
 
 
-def test_number_of_attendees_not_completed_course() -> None:
-    attendees = [Attendee(name='abc',email='abc@ibehave.com',certificate=True),Attendee(name='def',email='def@gmail.com',certificate=True),Attendee(name='ghi',email='ghi@xyz.com',certificate=False)]
-    workshop = {'wid':attendees}
-    repo = InMemoryAttandeeRepo(workshop=workshop)
+def test_zoom_number_of_attendees_not_completed_course() -> None:
+    attendees = [Attendee(name='abc',email='abc@ibehave.com',attendance=True),Attendee(name='def',email='def@gmail.com',attendance=True),Attendee(name='ghi',email='ghi@xyz.com',attendance=False)]
+    part_report = {'participants': [
+            {'status': 'in_meeting', 'name': 'abc',
+                'duration': 120, 'user_email': 'abc@ibehave.com'},
+            {'status': 'in_meeting', 'name': 'def',
+                'duration': 100, 'user_email': 'def@gmail.com'},
+            {'status': 'in_meeting', 'name': 'ghi',
+                'duration': 10, 'user_email': 'ghi@gmail.com'}                
+        ]}
+
+    api = Mock(ZoomAttendanceApi)
+    api.get_zoom_participant_report.return_value = part_report
+    repo = ZoomAttendeeRepo(zoom_api=api)
     attendance_workflows = AttendanceWorkflows(repo)
 
-    attendance_report = attendance_workflows.list_all_attendees(workshop_id='wid')
-
+    attendance_report = attendance_workflows.list_all_attendees(workshop_id=1234)
     assert len(attendance_report.list_unsuccessful_attendees) == 1
-
-# ZoomRepo testing
-# def test_length_attendees():
-#     attendees = [Attendee(name='abc',email='abc@ibehave.com',certificate=True),Attendee(name='def',email='def@gmail.com',certificate=True),Attendee(name='ghi',email='ghi@xyz.com',certificate=False)]
-#     part_report = {'participants': [
-#             {'status': 'in_meeting', 'name': 'abc',
-#                 'duration': 120, 'user_email': 'abc@ibehave.com'},
-#             {'status': 'in_meeting', 'name': 'def',
-#                 'duration': 100, 'user_email': 'def@gmail.com'},
-#             {'status': 'in_meeting', 'name': 'ghi',
-#                 'duration': 10, 'user_email': 'ghi@gmail.com'}                
-#         ]}
-
-#     repo = ZoomAttendeeRepo()
-#     attendance_workflows = AttendanceWorkflows(repo)
-
-#     with patch('ZoomAttendeeRepo.get_zoom_participant_report', part_report):
-#         attendance_report = attendance_workflows.list_all_attendees(token='1231414', meeting_id='1231')
-
-#     assert attendance_report.total_attendees == len(attendees)
 
 
 ##################
 class Attendee(NamedTuple):
     name: str
     email: str
-    certificate: bool
+    attendance: bool
 
 class AttendanceReport(NamedTuple):
     attendees: List[Attendee]
@@ -79,12 +86,12 @@ class AttendanceReport(NamedTuple):
     
     @property
     def list_successful_attendees(self) -> List[Attendee]:
-        return [attendee for attendee in self.attendees if attendee.certificate]
+        return [attendee for attendee in self.attendees if attendee.attendance]
         
 
     @property
     def list_unsuccessful_attendees(self) -> List[Attendee]:
-        return [attendee for attendee in self.attendees if not attendee.certificate]
+        return [attendee for attendee in self.attendees if not attendee.attendance]
 
 
 # interface
@@ -103,53 +110,35 @@ class AttendanceWorkflows(NamedTuple):
     
 
 # Implementations
-class InMemoryAttandeeRepo(AttendanceRepo):
-    def __init__(self, workshop: Dict[str,List[Attendee]]) -> None:
-        self.workshop = workshop
+class ZoomAttendanceApi:
+    @staticmethod
+    def get_zoom_participant_report(access_token:str, meeting_id: str, params:Dict = None):
+        url = f"https://api.zoom.us/v2/report/meetings/{meeting_id}/participants"
+        header = {
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        response = requests.get(url, headers=header, params=params)
+        response.raise_for_status()
+        return response.json()    
+
+class ZoomAttendeeRepo(AttendanceRepo):
+    def __init__(self, zoom_api: ZoomAttendanceApi) -> None:
+        self.api = zoom_api
+        self.access_token = 'ACCESS TOKEN'
 
     def list_all_attendees(self,workshop_id:str) -> List[str]:
-        return self.workshop[workshop_id]
+        report = self.api.get_zoom_participant_report(access_token=self.access_token,meeting_id=workshop_id)
+        participants = report['participants']
+        attendees = []
+        max_duration = np.nanmax([participant['duration'] for participant in participants if participant['status'] == 'in_meeting'])
+        attendance = False
+        for participant in participants:
+            if participant['status'] == 'in_meeting':
+                if participant['duration'] >= 0.75*max_duration:
+                    attendance = True
+                else:
+                    attendance = False
+            attendees.append(Attendee(name=participant['name'],email=participant['user_email'],attendance=attendance))
+        return attendees
 
-# class ZoomAttendeeRepo(AttendanceRepo):
-
-#     def get_zoom_participant_report(self, access_token:str, workshop_id: str):
-#         url = f"https://api.zoom.us/v2/report/meetings/{workshop_id}/participants"
-#         report = get_json_from_zoom_request(url=url, access_token=access_token)
-#         return report
-
-
-#     def list_all_attendees(self, access_token:str,workshop_id: str) -> List[Attendee]:
-#         participants = self.get_zoom_participant_report(access_token=access_token,workshop_id=workshop_id)['participants']
-
-#         names = []
-#         durations = []
-#         emails = []
-        
-#         max_duration = np.max([participant['duration']/60. for participant in participants])
-
-#         attendees = []
-#         for participant in participants:
-#             attendee = Attendee()
-#             if participant['status'] == 'in_meeting':
-#                 attendee.name = participant['name']
-#                 attendee.email = participant['user_email']
-#                 if participant['duration']/60. >= 0.75*max_duration:
-#                     attendee.certificate = True
-#                 else:
-#                     attendee.certificate = False
-#             attendees.append(attendee)
-#         return attendees
-
-
-#         return output
-    
-
-# ######################
-# def get_json_from_zoom_request(url: str, access_token: str, params: Dict = None) -> Union[List, Dict]:
-#     header = {
-#         'Authorization': f"Bearer {access_token}"
-#     }
-
-#     response = requests.get(url, headers=header, params=params)
-#     response.raise_for_status()
-#     return response.json()    
