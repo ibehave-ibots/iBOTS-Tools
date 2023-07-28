b9c517804e3967bed23d7ad91dba1822d1c183c9
Sangeetha <an.sangeetha@gmail.com>
2023-07-28 18:05:12 +0530

    fixed attendance bug, same name problem, adding duration of same attendee with different names
    Co-authored-by: Nicholas A. Del Grosso <delgrosso.nick@gmail.com>

--- course_attendance_service/course_attendance_service/workflows/make_attendance_report/attendance_workflows.py
+++ course_attendance_service/course_attendance_service/workflows/make_attendance_report/attendance_workflows.py
@@ -2,24 +2,27 @@
 # contains methods calling an abstract AttendeeRepo interface
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List, NamedTuple, Union
+from dataclasses import dataclass
+from typing import Any, Dict, List, NamedTuple, Optional, Union
+from collections import defaultdict
 import numpy as np
 import requests
 from unittest.mock import Mock
 
+@dataclass
 class Attendee:
-    def __init__(self, name:str, email:str, attendance: bool = None, duration: float = None):
-        self.name = name
-        self.email = email
-        self.attendance = attendance
-        self.duration = duration
+    name: str
+    email: str
+    attendance: Optional[bool] = None
+    duration: Optional[float] = None
 
 class AttendanceReport(NamedTuple):
     attendees: List[Attendee]
 
     @property
     def total_attendees(self) -> int:
-        return len(self.attendees)
+        unique_attendees = {attendee.email for attendee in self.attendees}
+        return len(unique_attendees)
     
     @property
     def list_successful_attendees(self) -> List[Attendee]:
@@ -41,8 +44,27 @@ class AttendanceRepo(ABC):
 class AttendanceWorkflows(NamedTuple):
     attendee_repo: AttendanceRepo
 
-    def list_all_attendees(self, workshop_id: str) -> List[Attendee]:
+    def list_all_attendees(self, workshop_id: str) -> AttendanceReport:
         attendees = self.attendee_repo.list_all_attendees(workshop_id=workshop_id)
+        unique_attendees = defaultdict(list)
+        for attendee in attendees:
+            unique_attendees[attendee.email].append(attendee) 
+
+
+        final_attendees = []
+        for unique_attendee_group in unique_attendees.values():
+            first_attendee = unique_attendee_group[0]
+            total_duration = sum(attendee.duration for attendee in unique_attendee_group if attendee.duration is not None)
+            total_duration = None if total_duration == 0. else total_duration 
+            final_attendee = Attendee(
+                name = first_attendee.name,
+                email = first_attendee.email,
+                attendance = first_attendee.attendance,
+                duration = total_duration,
+            )
+            final_attendees.append(final_attendee)
+
+        attendees = final_attendees
         non_none_durations = [attendee.duration for attendee in attendees if attendee.duration is not None]
         
         if non_none_durations:
--- course_attendance_service/course_attendance_service/workflows/make_attendance_report/test_attendance_workflows.py
+++ course_attendance_service/course_attendance_service/workflows/make_attendance_report/test_attendance_workflows.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from .zoom_attendance_api import ZoomAttendanceApi
+from .zoom_attendance_api import ZoomAttendanceApi, ZoomParticipantsResponseData
 
 from .zoom_attendance_repo import ZoomAttendeeRepo
 from .attendance_workflows import Attendee, AttendanceWorkflows
@@ -18,7 +18,10 @@ def test_length_attendees():
     assert attendance_report.total_attendees == len(attendees)
 
 def test_number_of_candidates_who_got_attendance() -> None:
-    attendees = [Attendee(name='abc',email='abc@ibehave.com',attendance=True),Attendee(name='def',email='def@gmail.com',attendance=True),Attendee(name='ghi',email='ghi@xyz.com',attendance=False)]
+    attendees = [
+        Attendee(name='abc',email='abc@ibehave.com',attendance=True),
+        Attendee(name='def',email='def@gmail.com',attendance=True),
+        Attendee(name='ghi',email='ghi@xyz.com',attendance=False)]
     workshop = {'wid':attendees}
     repo = InMemoryAttandeeRepo(workshop=workshop)
     attendance_workflows = AttendanceWorkflows(repo)
@@ -79,7 +82,7 @@ def test_zoom_number_of_candidates_who_got_attendance() -> None:
     assert len(attendance_report.list_successful_attendees) == 2
 
 
-def test_zoom_number_of_attendees_not_getting_attendance() -> None:
+def test_zoom_number_of_attendees_not_getting_attendance():
     part_report = {'participants': [
             {'status': 'in_meeting', 'name': 'abc',
                 'duration': 120, 'user_email': 'abc@ibehave.com'},
@@ -97,3 +100,19 @@ def test_zoom_number_of_attendees_not_getting_attendance() -> None:
     attendance_report = attendance_workflows.list_all_attendees(workshop_id=1234)
     assert len(attendance_report.list_unsuccessful_attendees) == 1
 
+
+def test_same_participant_is_not_counted_more_than_once_if_they_leave_and_rejoin():
+    part_report: ZoomParticipantsResponseData = {'participants': [
+        {'status': 'in_meeting', 'name': 'Mo',       'user_email': 'mo@mo.com', 'duration': 10},
+        {'status': 'in_meeting', 'name': 'Mohammad', 'user_email': 'mo@mo.com', 'duration': 20},
+    ]}
+    api = Mock(ZoomAttendanceApi)
+    api.get_zoom_participant_report.return_value = part_report
+    repo = ZoomAttendeeRepo(zoom_api=api)
+    attendance_workflows = AttendanceWorkflows(repo)
+    attendance_report = attendance_workflows.list_all_attendees(workshop_id=1234)
+    
+    assert attendance_report.total_attendees == 1
+    assert len(attendance_report.attendees) == 1 
+
+    
