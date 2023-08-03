d18af87a64729b0c6f9df2fd5faba647ddae773e
Sangeetha <an.sangeetha@gmail.com>
2023-08-03 19:54:21 +0530

    added zoom_repo and inmemory_repo fixtures and refactored workflow_test
    Co-authored-by: Nicholas A. Del Grosso <delgrosso.nick@gmail.com>

--- course_attendance_service/course_attendance_service/workflows/make_attendance_report/workflow_test.py
+++ course_attendance_service/course_attendance_service/workflows/make_attendance_report/workflow_test.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+import pytest
 from .core.attendance_workflows import AttendanceWorkflows
 from .adapters.in_memory_repo import InMemoryAttendeeRepo
 from .adapters.example_attendees import *
@@ -6,93 +8,88 @@ from .external.zoom_api import ZoomAttendanceApi, ZoomParticipantsResponseData, 
 from .adapters.zoom_attendance_repo import ZoomAttendeeRepo
 from unittest.mock import Mock
 
+start_time = datetime(2023, 7, 7, hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+end_time = datetime(2023, 7, 7, hour=1, minute=1, second=59, microsecond=0, tzinfo=None)
 
 
-def attendees_details():
+
+
+@pytest.fixture()
+def inmemory_repo() -> InMemoryAttendeeRepo:
     attendees = [attendee1, attendee2, attendee3, attendee4, attendee5, attendee6]
-    return attendees
+    inmemory_repo = InMemoryAttendeeRepo(start_time=start_time,end_time=end_time,attendees=attendees)
+    return inmemory_repo
 
-def prep_zoom_repo():
+@pytest.fixture()
+def zoom_repo():
+    meeting_data: ZoomMeetingData = {'start_time':start_time,
+                                    'end_time':end_time}
+    part_report: ZoomParticipantsResponseData = {
+        'participants': [{'status': 'in_meeting', 'name': attendee.name, 'user_email': attendee.email,
+                        'join_time': attendee.join_start, 'leave_time': attendee.join_end}
+                        for attendee in [attendee1, attendee2, attendee3, attendee4, attendee5, attendee6]
+                        ]
+    }
     api = Mock(ZoomAttendanceApi)
     api.get_past_meeting_details.return_value = meeting_data
     api.get_zoom_participant_report.return_value = part_report
-    repo = ZoomAttendeeRepo(zoom_api=api)
-    attendance_workflows = AttendanceWorkflows(repo)
-    return attendance_workflows
+    zoom_repo = ZoomAttendeeRepo(zoom_api=api)
+    return zoom_repo
 
 
 
-start_time=datetime(2023, 7, 7, hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
-end_time=datetime(2023, 7, 7, hour=1, minute=1, second=59, microsecond=0, tzinfo=None)
 
-meeting_data: ZoomMeetingData = {'start_time':start_time,
-                                'end_time':end_time}
-
-part_report: ZoomParticipantsResponseData = {
-    'participants': [{'status': 'in_meeting', 'name': attendee.name, 'user_email': attendee.email,
-                      'join_time': attendee.join_start, 'leave_time': attendee.join_end}
-                     for attendee in [attendee1, attendee2, attendee3, attendee4, attendee5, attendee6]
-                    ]
-}
-
-# InMemory testing
-def test_number_of_attendees():
-    attendees = attendees_details()
-
-    repo = InMemoryAttendeeRepo(start_time=start_time,end_time=end_time,attendees=attendees)
-    attendance_workflows = AttendanceWorkflows(repo)
-
+####
+def test_number_of_attendees(inmemory_repo):
+    attendance_workflows = AttendanceWorkflows(inmemory_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id='session_123')
     assert attendance_report.total_attendees == 3
 
-def test_zoom_number_of_attendees():
-    attendance_workflows = prep_zoom_repo()
 
+def test_zoom_number_of_attendees(zoom_repo):
+    attendance_workflows = AttendanceWorkflows(zoom_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id=1234)
-
     assert attendance_report.total_attendees == 3
 
 
-def test_number_of_candidates_who_got_attendance() -> None:
-    attendees = attendees_details()
 
-    repo = InMemoryAttendeeRepo(start_time=start_time,end_time=end_time,attendees=attendees)
-    attendance_workflows = AttendanceWorkflows(repo)
-
+###
+def test_number_of_candidates_who_got_attendance(inmemory_repo) -> None:
+    attendance_workflows = AttendanceWorkflows(inmemory_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id='session_123')
-
     assert len(attendance_report.list_successful_attendees) == 2
 
-def test_zoom_number_of_candidates_who_got_attendance() -> None:
-    attendance_workflows = prep_zoom_repo()
+def test_zoom_number_of_candidates_who_got_attendance(zoom_repo) -> None:
+    attendance_workflows = AttendanceWorkflows(zoom_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id=1234)
     assert len(attendance_report.list_successful_attendees) == 2
 
 
-def test_number_of_attendees_not_getting_attendance() -> None:
-    attendees = attendees_details()
 
-    repo = InMemoryAttendeeRepo(start_time=start_time,end_time=end_time,attendees=attendees)
-    attendance_workflows = AttendanceWorkflows(repo)
-
+####
+def test_number_of_attendees_not_getting_attendance(inmemory_repo) -> None:
+    attendance_workflows = AttendanceWorkflows(inmemory_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id='session_123')
-
     assert len(attendance_report.list_unsuccessful_attendees) == 1
 
-def test_zoom_number_of_attendees_not_getting_attendance():
-    attendance_workflows = prep_zoom_repo()
+def test_zoom_number_of_attendees_not_getting_attendance(zoom_repo):
+    attendance_workflows = AttendanceWorkflows(zoom_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id=1234)
     assert len(attendance_report.list_unsuccessful_attendees) == 1
 
-def test_duration_of_attendee_with_only_one_appearance():
-    attendees = attendees_details()
-    repo = InMemoryAttendeeRepo(start_time=start_time,end_time=end_time,attendees=attendees)
-    attendance_workflow = AttendanceWorkflows(repo)
+
+
+
+####
+def test_duration_of_attendee_with_only_one_appearance(inmemory_repo):
+    attendance_workflow = AttendanceWorkflows(inmemory_repo)
     attendance_report = attendance_workflow.make_attendance_report(session_id='session_xyz')
-
     assert attendance_report.report[0].total_duration == 3719.
     assert attendance_report.report[1].total_duration == 900.
 
+
+
+####
 def test_duration_not_added_multiple_devices():
     attendees = [attendee3, attendee4, attendee5, attendee6]
 
@@ -106,15 +103,18 @@ def test_duration_not_added_multiple_devices():
     assert len(attendance_report.list_unsuccessful_attendees) == 0
     assert attendance_report.report[0].total_duration == 3300.
 
-def test_zoom_same_participant_is_not_counted_more_than_once_if_they_leave_and_rejoin():
-    attendance_workflows = prep_zoom_repo()
+
+####
+def test_zoom_same_participant_is_not_counted_more_than_once_if_they_leave_and_rejoin(zoom_repo):
+    attendance_workflows = AttendanceWorkflows(zoom_repo)
     attendance_report = attendance_workflows.make_attendance_report(session_id=1234)    
     assert attendance_report.total_attendees == 3
 
-def test_zoom_duration_not_added_multiple_devices():
-    attendance_workflows = prep_zoom_repo()
-    attendance_report = attendance_workflows.make_attendance_report(session_id=1234)
 
+def test_zoom_duration_not_added_multiple_devices(zoom_repo):
+    attendance_workflows = AttendanceWorkflows(zoom_repo)
+    attendance_report = attendance_workflows.make_attendance_report(session_id=1234)
+    
     assert attendance_report.total_attendees == 3
     assert len(attendance_report.list_successful_attendees) == 2
     assert len(attendance_report.list_unsuccessful_attendees) == 1
