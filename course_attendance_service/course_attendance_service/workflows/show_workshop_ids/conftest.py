a5382adda8c174336fc4f0266b3d6cc4d9b18501
Nick A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-04 00:18:59 +0200

    test both repo implementations

---
+++ course_attendance_service/course_attendance_service/workflows/show_workshop_ids/conftest.py
@@ -0,0 +1,40 @@
+from unittest.mock import Mock
+
+import pytest
+
+from .core.workflow import WorkshopRepo
+from .adapters.repo_inmemory import InMemoryWorkshopRepo
+from .adapters.repo_zoom import ZoomWorkshopRepo
+from .external.zoom_api import ZoomRestApi
+
+    
+@pytest.fixture
+def zoom_workshop_repo():
+    api = Mock(ZoomRestApi)
+    api.list_users_in_group.return_value = ['emma', 'addy']
+    api.list_scheduled_meetings_of_user.side_effect = [
+        {'meetings': ['AA', 'BB']},
+        {'meetings': ['CC']},
+    ]
+    repo = ZoomWorkshopRepo(zoom_api=api)
+    return repo
+
+      
+@pytest.fixture
+def inmemory_workshop_repo() -> InMemoryWorkshopRepo:
+    given_workshops = [
+        {'id': 'AA'}, 
+        {'id': 'BB'}, 
+        {'id': 'CC'}, 
+    ]
+    repo = InMemoryWorkshopRepo(workshops=given_workshops)
+    return repo
+  
+  
+@pytest.fixture(params=['in-memory', 'zoom'])
+def workshop_repo(request, zoom_workshop_repo, inmemory_workshop_repo) -> WorkshopRepo:
+    if request.param == 'in-memory':
+        return inmemory_workshop_repo
+    elif request.param == 'zoom':
+        return zoom_workshop_repo
+        
--- course_attendance_service/course_attendance_service/workflows/show_workshop_ids/test_workflow.py
+++ course_attendance_service/course_attendance_service/workflows/show_workshop_ids/test_workflow.py
@@ -1,34 +1,21 @@
-from datetime import datetime
-from random import randint, choices, seed
-from string import ascii_letters
 from textwrap import dedent
 from unittest.mock import Mock
 
-
-
 from .core.workflow import ListWorkshopsWorkflows
-from .adapters.repo_inmemory import InMemoryWorkshopRepo
+from .core.workshop_repo import WorkshopRepo
 from .adapters.console_printer import ConsoleWorkshopListPresenter
 from .external.console import Console
 
-rand_letters = lambda: ''.join(choices(ascii_letters, k=4))
-rand_date = lambda: datetime(year=randint(1900, 2100), month=randint(1, 12), day=randint(1, 28))
-    
-                               
-def test_list_all_workshops_ids():
-    
-    given_workshops = [
-        {'id': 'AA'}, 
-        {'id': 'BB'}, 
-        {'id': 'CC'}, 
-    ]
-    
-    repo = InMemoryWorkshopRepo(workshops=given_workshops)
+
+      
+def test_list_all_workshops_ids(workshop_repo: WorkshopRepo):
+    workflows = ListWorkshopsWorkflows(workshop_repo=workshop_repo)
     console = Mock(Console)
-    presenter = ConsoleWorkshopListPresenter(console=console)
     
-    workflows = ListWorkshopsWorkflows(workshop_repo=repo)
-    workflows.show_all_workshops(presenter=presenter)
+    # When
+    workflows.show_all_workshops(presenter=ConsoleWorkshopListPresenter(console=console))
+    
+    # Then
     workshop_id_text = console.print.call_args[0][0]
     expected_text = dedent("""
     AA
@@ -36,3 +23,5 @@ def test_list_all_workshops_ids():
     CC
     """).lstrip()
     assert workshop_id_text == expected_text
+
+
