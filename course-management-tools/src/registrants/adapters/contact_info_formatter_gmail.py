56f91bf2bf8fba762a066d3ab68ec4075f0c96a1
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-08-08 16:55:55 +0000

    Simplify gmail formetter

--- course-management-tools/src/registrants/adapters/contact_info_formatter_gmail.py
+++ course-management-tools/src/registrants/adapters/contact_info_formatter_gmail.py
@@ -4,9 +4,6 @@ from ..core.contact_info_formatter import ContactInfoFormatter
 class GmailContactInfoFormatter(ContactInfoFormatter):
     def format_contact_info(self, registrants):
         contact_info = ""
-        for registrant_idx, registrant in enumerate(registrants):
-            contact_info += f"{registrant.email}"
-            is_not_last_registrant = registrant_idx < len(registrants) - 1
-            if is_not_last_registrant:
-                contact_info += ",\n"
+        for registrant in registrants:
+            contact_info += f"{registrant.email},\n"
         return contact_info
