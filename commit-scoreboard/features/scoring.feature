cab4df8d8bbd8788d9fb7f802eba9029d8be9d38
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-17 22:57:43 +0200

    parametrized test

--- commit-scoreboard/features/scoring.feature
+++ commit-scoreboard/features/scoring.feature
@@ -1,16 +1,13 @@
 Feature: Calculate Score based on Number of Relative Commits
 
-Scenario: 
-    Given the "team-1" branch is 3 commits ahead of the reference "main" branch
-    When the scores are calculated for teams "team1"
-    Then "team-1" is shown to have a score of 3
+Scenario Outline: A team is ahead of a branch by different amounts: 
+    Given the <team> branch is <commits> commits ahead of the reference <ref> branch
+    When the scores are calculated for teams <team> against reference branch <ref>
+    Then <team> is shown to have a score of <score>
 
-@skip
-Scenario: Two teams make different numbers of commits
-    Given a team has made 7 commits on the "team-1" branch
-    And a team has made 5 commits on the "team-4" branch
-    When the scores are calculated
-    Then "team-1" is shown to have a score of 7
-    And "team-4" is shown to have a score of 5
+    Examples:
+        | team | ref | commits | score |
+        | team-1 | main | 3 | 3 |
+        | team-4 | dev | 6 | 6 |
 
 
--- commit-scoreboard/features/steps/scoring.py
+++ commit-scoreboard/features/steps/scoring.py
@@ -8,6 +8,14 @@ class VersionControlRepo(ABC):
     @abstractmethod
     def count_commits_ahead(self, ref: str, target: str) -> int: ...
 
+class DummyVersionControlRepo(VersionControlRepo):
+    def __init__(self, **commits: dict[str, int]) -> None:
+        self.branch_commits = commits
+
+    def count_commits_ahead(self, ref: str, target: str) -> int:
+        return self.branch_commits[target] - self.branch_commits[ref]
+        
+
 class Score(NamedTuple):
     score: int
 
@@ -21,7 +29,6 @@ class CalculateScores:
     version_control_repo: VersionControlRepo
     scoreboard: ScoreboardPresenter
 
-
     def run(self, ref_branch: str, target_branches: list[str]) -> None:
         target_branch = target_branches[0]
         commits_ahead = self.version_control_repo.count_commits_ahead(ref=ref_branch, target=target_branch)
@@ -29,29 +36,27 @@ class CalculateScores:
         self.scoreboard.present(scores=scores)
 
         
-        
-
-@given(u'the "team-1" branch is 3 commits ahead of the reference "main" branch')
-def step_impl(context):
-
-    # Setup
-    context.reference_branch = "main"
-    vcs = Mock(VersionControlRepo)
-    vcs.count_commits_ahead.return_value = 3
-
-    scoreboard = Mock(ScoreboardPresenter)
-    context.calculate_scores = CalculateScores(
-        version_control_repo=vcs,
-        scoreboard=scoreboard
-    ).run
-    context.scoreboard = scoreboard
-
-@when(u'the scores are calculated for teams "team1"')
-def step_impl(context):
-    context.calculate_scores(ref_branch="main", target_branches=['team-1'])
+    
 
 
-@then(u'"team-1" is shown to have a score of 3')
-def step_impl(context):
+@given(u'the {team} branch is {n:d} commits ahead of the reference {ref} branch')
+def step_impl(context, team, ref, n):
+    branches = {ref: 0, team: n}
+    context.vcs = DummyVersionControlRepo(**branches)
+    
+    
+
+@when(u'the scores are calculated for teams {team} against reference branch {ref}')
+def step_impl(context, team, ref):
+    context.scoreboard = Mock(ScoreboardPresenter)
+    CalculateScores(
+        version_control_repo=context.vcs,
+        scoreboard= context.scoreboard
+    ).run(ref_branch=ref, target_branches=[team])
+    
+
+
+@then(u'{team} is shown to have a score of {x:d}')
+def step_impl(context, team, x):
     scoreboard = context.scoreboard.present.call_args[1]
-    assert scoreboard['scores']['team-1'].score == 3
+    assert scoreboard['scores'][team].score == x
