80346dbbcb05095e4e0f4721587c5e7b662ac4a8
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-18 13:14:41 +0200

    new console view

--- commit-scoreboard/main.py
+++ commit-scoreboard/main.py
@@ -9,18 +9,39 @@ from scoreboard.core.app import AppModel, Application, ScoreboardView, TeamSetti
 
 class ConsoleView(ScoreboardView):
     def update(self, model: AppModel) -> None:
-        # print("Reference:", model.reference_branch)
-        # print("Settings:")
-        # pprint(dict(model.settings))
-        # print("Status:")
         pprint(dict(model.statuses))
-        # print('')
 
 
+class ConsoleView2(ScoreboardView):
+    def __init__(self) -> None:
+        self.first_shown = False
+
+    def _show_header(self, model: AppModel):
+        line = '|'
+        for team, status in model.statuses.items():
+            entry = f' {team:<8}|'
+            line += entry
+        print(line)
+
+    def update(self, model: AppModel) -> None:
+        if not self.first_shown:
+            self._show_header(model=model)
+            self.first_shown = True
+
+        line = '|'
+        for team, status in model.statuses.items():
+            ding = "*" if status.play_sound else ""
+            entry = f' {str(status.points) + ding:<8}|'
+            line += entry
+        print(line)
+
+        
+        
 
 def run(app: Application) -> None:
     while True:    
-        app.vcs_repo.branch_commits['team-1'] += randint(0, 2)
+        for team in app.model.statuses:
+            app.vcs_repo.branch_commits[team] += randint(0, 2)
         app.update_points()
         time.sleep(1)
 
@@ -28,17 +49,19 @@ def run(app: Application) -> None:
 if __name__ == '__main__':
 
     app = Application(
-        view=ConsoleView(),
+        view=ConsoleView2(),
         model=AppModel(
             settings={
-                'team-1': TeamSettings(interval=9),
+                'team-1': TeamSettings(interval=6),
+                'team-2': TeamSettings(interval=6),
             },
             statuses={
                 'team-1': TeamState(points=0),
+                'team-2': TeamState(points=0),
             },
             reference_branch='main',
         ),
-        vcs_repo=DummyVersionControlRepo(**{'main': 0, 'team-1': 0}),
+        vcs_repo=DummyVersionControlRepo(**{'main': 0, 'team-1': 0, 'team-2': 0}),
     )
 
     run(app)
