e4b3d6c02a5919622210a082a033806236f55c0d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-18 09:32:57 +0200

    removed observer pattern

--- commit-scoreboard/features/steps/ding.py
+++ commit-scoreboard/features/steps/ding.py
@@ -1,7 +1,7 @@
 from unittest.mock import Mock
 from behave import given, when, then
 
-from scoreboard.app import TeamSettings, TeamState, AppState    
+from scoreboard.app import TeamSettings, TeamState, Application    
 
 
 @given(u'the interval setting for {team} is set to {interval:d}')
@@ -13,21 +13,21 @@ def step_impl(context, team, interval):
 
 @given(u'that {team} already had {points:d} points')
 def step_impl(context, team, points):
-    context.app_state = AppState(
+    context.display = Mock()
+    context.app = Application(
         statuses={team: TeamState(points=points)},
         settings=context.settings,
+        view = context.display
     )
-    context.display = Mock()
-    context.app_state.register_observer(context.display)
 
 
 @when(u'the {team} gets {points:d} points')
 def step_impl(context, team, points):    
-    context.app_state.update_points(team, points)
+    context.app.update_points(team, points)
 
 
 @then(u'the {team} ding sound should be {status}')
 def step_impl(context, team, status):
     is_on = {'on': True, 'off': False}[status]
-    view = context.display.call_args[1]['statuses']
-    assert view[team].play_sound == is_on
+    display = context.display.update.call_args[1]['statuses']
+    assert display[team].play_sound == is_on
--- commit-scoreboard/scoreboard/app.py
+++ commit-scoreboard/scoreboard/app.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import NamedTuple, Protocol
     
@@ -11,19 +12,21 @@ class TeamState(NamedTuple):
     play_sound: bool = False
 
 
-class StateObserver(Protocol):
-    def __call__(self, statuses: dict[str, TeamState]): ...
-
-
 def should_play_sound(played_sound_before: bool, interval: int, old_score: int, new_score: int) -> bool:
     return not played_sound_before and new_score // interval > old_score // interval
 
 
+class ScoreboardView(ABC):
+
+    @abstractmethod
+    def update(self, statuses: dict[str, TeamState]) -> None: ...
+
 @dataclass
-class AppState:
+class Application:
+    view: ScoreboardView
     statuses: dict[str, TeamState] = field(default_factory=dict)
     settings: dict[str, TeamSettings] = field(default_factory=dict)
-    observers: set[StateObserver] = field(default_factory=set)
+    
 
     def update_points(self, team: str, points: int) -> None:
         
@@ -36,12 +39,5 @@ class AppState:
             new_score=points,
         )
         self.statuses[team] = TeamState(points=points, play_sound=play_sound)
-        self.notify_observers()
-
-    def register_observer(self, observer: StateObserver) -> None:
-        self.observers.add(observer)
-
-    def notify_observers(self) -> None:
-        for observer in self.observers:
-            observer(statuses=self.statuses)
+        self.view.update(statuses=self.statuses)
 
