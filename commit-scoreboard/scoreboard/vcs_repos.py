0574cd997a5e38475b29a1139fb6493d406b0179
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-18 09:06:38 +0200

    split src from tests for git repo

--- commit-scoreboard/scoreboard/vcs_repos.py
+++ commit-scoreboard/scoreboard/vcs_repos.py
@@ -1,3 +1,5 @@
+import git
+
 from .score_calculation import VersionControlRepo
 
 
@@ -8,3 +10,12 @@ class DummyVersionControlRepo(VersionControlRepo):
     def count_commits_ahead(self, ref: str, target: str) -> int:
         return self.branch_commits[target] - self.branch_commits[ref]
         
+
+
+class GitVersionControlRepo(VersionControlRepo):
+    def __init__(self, git_repository: git.Repo):
+        self.git_repository = git_repository
+
+    def count_commits_ahead(self, ref: str, target: str) -> int:
+        return len(list(self.git_repository.iter_commits(f"{ref}..{target}")))
+
--- commit-scoreboard/tests/test_git_vscrepo.py
+++ commit-scoreboard/tests/test_git_vscrepo.py
@@ -1,17 +1,7 @@
 from unittest.mock import Mock
 import git
 
-from scoreboard.workflow import VersionControlRepo
-
-
-class GitVersionControlRepo(VersionControlRepo):
-    def __init__(self, git_repository: git.Repo):
-        self.git_repository = git_repository
-
-    def count_commits_ahead(self, ref: str, target: str) -> int:
-        return len(list(self.git_repository.iter_commits(f"{ref}..{target}")))
-
-
+from scoreboard.vcs_repos import GitVersionControlRepo
 
 
 def test_can_count_commits_from_git_repo():
