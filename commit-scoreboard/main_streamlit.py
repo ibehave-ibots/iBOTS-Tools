d71123d40f7e37fc4fbcec7cd9c40c6e4b992a28
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-18 19:24:15 +0200

    Add text-based barplot streamlit layout
    Co-authored-by: Mohammad Bashiri <mohammadbashiri93@gmail.com>

--- commit-scoreboard/main_streamlit.py
+++ commit-scoreboard/main_streamlit.py
@@ -35,7 +35,20 @@ class TextStreamlitTeamScoreComponent(TeamScoreComponent):
         self.score_widget.text(score)
 
 
+class TextBarStreamlitTeamScoreComponent(TeamScoreComponent):
 
+    def __init__(self, fontsize: str = "20px"):
+        self.fontsize = fontsize
+        col1, col2 = st.columns([1, 8])
+        with col1:
+            self.name_widget = st.empty()
+        with col2:
+            self.score_widget = st.empty()
+    
+    def render(self, name: str, score: int):
+        self.name_widget.markdown(f'<p style="font-size:{self.fontsize};border-radius:2%;">{name}</p>', unsafe_allow_html=True)
+        score_with_bar = "".join(["|" for _ in range(score)]) + f" {score}"
+        self.score_widget.markdown(f'<p style="font-size:{self.fontsize};border-radius:2%;">{score_with_bar}</p>', unsafe_allow_html=True)
 
 class TextScoreboardView(ScoreboardView):
 
@@ -52,7 +65,7 @@ class TextScoreboardView(ScoreboardView):
 
         
 
-view = TextScoreboardView()
+view = TextScoreboardView(component_factory=TextBarStreamlitTeamScoreComponent)
 model=AppModel(
     settings={
         'team-1': TeamSettings(interval=6),
@@ -61,7 +74,7 @@ model=AppModel(
         'team-4': TeamSettings(interval=40),
     },
     statuses={
-        'team-1': TeamState(points=0),
+        'team-1': TeamState(points=30),
         'team-2': TeamState(points=5),
         'team-3': TeamState(points=0),
         'team-4': TeamState(points=22),
