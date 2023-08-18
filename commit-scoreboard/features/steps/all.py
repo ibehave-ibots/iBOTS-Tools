6191c7b0a568fe67a6e6cafb9cf35e3dd8f2efb8
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-18 11:48:55 +0200

    merged steps together

--- commit-scoreboard/features/steps/all.py
+++ commit-scoreboard/features/steps/all.py
@@ -24,3 +24,22 @@ def step_impl(context, team, status):
     is_on = {'on': True, 'off': False}[status]
     display = context.display.update.call_args[1]['model']
     assert display.statuses[team].play_sound == is_on
+
+
+
+@given(u'the {team} branch is {n:d} commits ahead of the reference {ref} branch')
+def step_impl(context, team, ref, n):
+    context.vcs.branch_commits = {ref: 0, team: n}
+    
+    
+@when(u'the scores are calculated for teams {team} against reference branch {ref}')
+def step_impl(context, team, ref):
+    context.app.model.statuses[team].active_branch = team
+    context.app.model.reference_branch = ref
+    context.app.update_points()
+    
+
+@then(u'{team} is shown to have a score of {x:d}')
+def step_impl(context, team, x):
+    display = context.display.update.call_args[1]['model']
+    display.statuses[team].points == x
