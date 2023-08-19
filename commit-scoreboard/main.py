6784b81b9b0f9497dbfd71a8f476cca30b95659b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-19 15:34:30 +0200

    shifted shared controller code into own module, shared by main scripts

--- commit-scoreboard/main.py
+++ commit-scoreboard/main.py
@@ -1,13 +1,12 @@
-from random import randint
-import time
-
+from scoreboard.controllers import run_simulation
+from scoreboard.core.app import AppModel, Application
 from scoreboard.adapters.vcs_repo_dummy import DummyVersionControlRepo
-from scoreboard.core.app import AppModel, Application, TeamSettings, TeamState
-
 from scoreboard.adapters.views_console import ConsoleView2
 from scoreboard.adapters.speaker_sounddevice import SounddeviceSpeaker
 
+
 branches = ['team-1', 'team-2']
+
 model = AppModel(reference_branch='main')
 for team in branches:
     model.add_team(team, interval=6, points=0)
@@ -19,12 +18,5 @@ app = Application(
     speaker=SounddeviceSpeaker(),
 )
 
-def run(app: Application, vcs: DummyVersionControlRepo) -> None:
-    while True:    
-        new_commits = {team: randint(0, 2) for team in app.model.team_names}
-        vcs.make_commits(**new_commits)
-        app.update()
-        time.sleep(1)
-
-run(app=app, vcs=app.vcs_repo)
+run_simulation(app=app, vcs=app.vcs_repo)
 
--- commit-scoreboard/main_streamlit.py
+++ commit-scoreboard/main_streamlit.py
@@ -1,39 +1,28 @@
-from random import randint
-import time
-
-import streamlit as st
-
+from scoreboard.controllers import run_simulation
 from scoreboard.core.app import AppModel, Application
 from scoreboard.core.scoreboard_view import ComponentScoreboardView
-
 from scoreboard.adapters.vcs_repo_dummy import DummyVersionControlRepo
 from scoreboard.adapters.speaker_sounddevice import SounddeviceSpeaker
 from scoreboard.adapters.views_streamlit import TextBarStreamlitTeamScoreComponent
 
 
-# Create View
-branch_names = [f'team-{n}' for n in range(1, 4)] 
+branches = [f'team-{n}' for n in range(1, 4)] 
 
 model = AppModel(reference_branch='main')
-for name in branch_names:
+for name in branches:
     model.add_team(name, points=0, interval=10)
     
+view = ComponentScoreboardView(component_factory=TextBarStreamlitTeamScoreComponent)
+view.init(model=model)
 
-dummy_vcs_repo = DummyVersionControlRepo(**{'main': 0} | {name: 0 for name in branch_names})
+app = Application(
+    view=view,
+    model=model,
+    vcs_repo=DummyVersionControlRepo(**{'main': 0} | {name: 0 for name in branches}),
+    speaker=SounddeviceSpeaker(),
+)
 
-if not st.session_state.get('app'):
-    view = ComponentScoreboardView(component_factory=TextBarStreamlitTeamScoreComponent)
-    view.init(model=model)
-    app = Application(
-        view=view,
-        model=model,
-        vcs_repo=dummy_vcs_repo,
-        speaker=SounddeviceSpeaker(),
-    )
-    st.session_state['app'] = app
-
-while True:    
-    new_commits = {team: randint(0, 2) for team in app.model.team_names}
-    dummy_vcs_repo.make_commits(**new_commits)
-    app.update()
-    time.sleep(1)
+run_simulation(
+    app=app,
+    vcs=app.vcs_repo,
+)
