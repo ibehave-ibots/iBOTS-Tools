1aecdde3c41d554a1f2bbe6069a0d5f742787d28
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-08-20 17:17:34 +0200

    round1 docs
    Co-authored-by: Nick A. Del Grosso <delgrosso.nick@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- round1.py
+++ round1.py
@@ -1,22 +1,22 @@
-code_reference = '''
-import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
-df = pd.read_csv('filename') 
+reference = '''
+st.write(df)
+st.metric('Age', 10)
+st.dataframe(df)
+st.data_editor(df)
+ 
 df['column_A'].min()  
 df['column_A'].max() 
 df['column_A'].mean() 
 df['column_A'].std() 
 df['column_A'].value_counts() 
 df['column_A'].unique()
+'''
 
 import streamlit as st                  # https://docs.streamlit.io/library/api-reference
+import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
+
 st.title('Title')
 st.subheader('Subheader') 
 st.text('Text')
 st.write('## Subheader')
-st.write(data)
-st.write(plot)
-st.metric('Age',10)  
-st.dataframe(data)
-st.data_editor(data) 
-st.bar_chart(data)
-'''
+df = pd.read_csv('data/round1.csv') 
