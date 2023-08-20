8eada440fbf11c197f724a355a1e4b787b15420b
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-08-20 17:29:50 +0200

    round2 docs Co-authored-by: Nick A. Del Grosso <delgrosso.nick@gmail.com> Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- round2.py
+++ round2.py
@@ -1,38 +1,32 @@
 code_reference = '''
-# pandas https://pandas.pydata.org/docs/reference/index.html
-df = pd.read_csv('filename') 
-df[df['column_A'] == n] 
-len(df) 
-df['column_A'].min()  
-df['column_A'].max() 
-df['column_A'].mean() 
-df['column_A'].std() 
-df.describe() 
-df['column_A'].describe() 
-df['column_A'].value_counts() 
-df.astype({'column_A': "category"}) 
-
-# seaborn https://seaborn.pydata.org/generated/seaborn.catplot.html
-sns.catplot(data=df,x='column_A',y='column_B',kind="bar") 
-sns.catplot(data=df,x='column_A',y='column_B',kind="bar",hue=column_C)    
-sns.catplot(data=df,x='column_A',y='column_B',kind="box") 
-sns.catplot(data=df,x='column_A',y='column_B',kind="violin") 
-sns.catplot(data=df,x='column_A',y='column_B',kind="point") 
-
-# streamlit https://docs.streamlit.io/library/api-reference
-st.title(your_title) 
-st.metric(label='metric label',value=value_to_display) 
-df 
-st.write(df) 
-st.dataframe(df) 
-
 # create and show plot
-sns.catplot(data=df,x='column_A',y='column_B',kind="bar")
+sns.catplot(data=df, x='column_A', y='column_B', kind="bar")
 st.write(plt)
 
-tab1, tab2 = st.tabs(tabs=['tab1_name', 'tab2_name']) 
-with tab1: 
-    st.text('I am in tab 1')
-with tab2: 
-    st.text('I am in tab 2')        
+# seaborn 
+sns.catplot(
+    data=df,
+    x='column_A',
+    y='column_B',
+    kind="bar",   # can also be "box", "violin", "point", "line", "boxen", "strip", "swarm" 
+    hue = 'column_C', # optional
+    row = 'column_D', # optional
+    col = 'column_E', # optional
+)        
 '''
+
+import seaborn as sns                   # https://seaborn.pydata.org/generated/seaborn.catplot.html
+import streamlit as st                  # https://docs.streamlit.io/library/api-reference
+import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/round2.csv')
+st.data_editor(df)
+
+sns.catplot(
+    data=df, 
+    x='Subject', 
+    y='Time', 
+    kind="bar",
+)
+st.write(plt)
