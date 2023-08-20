c2dcbd0c9a17218bddc2637509856ce24e78c8c0
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-08-20 13:36:49 +0200

    Make altair barplot nicer
    Co-authored-by: Mohammad Bashiri <mohammadbashiri93@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- commit-scoreboard/scoreboard/adapters/views_streamlit.py
+++ commit-scoreboard/scoreboard/adapters/views_streamlit.py
@@ -37,11 +37,11 @@ class TextBarStreamlitTeamScoreComponent(TeamScoreComponent):
 
 class BarPlotStreamlitTeamScoreComponent(TeamScoreComponent):
     def __init__(self):
-        # col1, col2 = st.columns([1, 8])
-        # with col1:
-        # self.name_widget = st.empty()
-        # with col2:
-        self.score_widget = st.empty()
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            self.name_widget = st.empty()
+        with col2:
+            self.score_widget = st.empty()
 
     def render(self, name: str, score:int):
         df = pd.DataFrame(
@@ -49,11 +49,18 @@ class BarPlotStreamlitTeamScoreComponent(TeamScoreComponent):
              'name':[name]}
         )
 
-        chart = alt.Chart(df).mark_bar().encode(
-            x=alt.X('score:Q', scale=alt.Scale(domain=(0,20))),
-            y=alt.Y('name')
+        bar_chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X('score:Q', scale=alt.Scale(domain=(0,20)), axis=alt.Axis(labels=True, title="", values=[0, 2, 4, 6, 8, 10], labelPadding=0)),
+            y=alt.Y('name', axis=alt.Axis(labels=False, title=""))
         )
 
-        # self.name_widget.text(name)
-        self.score_widget.write(chart)
+        text_chart = alt.Chart(df).mark_text(align='left', dx=3, color="white").encode(
+            x=alt.X('score:Q', scale=alt.Scale(domain=(0, 20))),
+            y=alt.Y('name', axis=alt.Axis(labels=False, title="")),
+            text='score'
+        )
 
+        chart = (bar_chart + text_chart)
+
+        self.name_widget.markdown(f"<p style='text-align: right;'>{name}</p>", unsafe_allow_html=True)
+        self.score_widget.write(chart)
