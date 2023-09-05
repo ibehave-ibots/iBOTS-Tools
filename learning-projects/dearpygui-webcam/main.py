d2a869f49da637d4de5f478b46872df8b597856b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-05 19:26:45 +0200

    moved to oop style

--- learning-projects/dearpygui-webcam/main.py
+++ learning-projects/dearpygui-webcam/main.py
@@ -1,42 +1,65 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 import cv2
 import numpy as np
 from dearpygui import dearpygui as dpg
 
-image_view = np.ones((480, 640, 4), dtype=np.float32)
-
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
-
-# Callback function to update the image texture
-def update_image():
-    ret, frame = cap.read()
-    if ret:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (640, 480))
-        frame = np.ascontiguousarray(frame)
-        image_view[:, :, :3] = frame / 255
 
 
+@dataclass
+class DearPyGuiView:
+    webcam: OpenCVWebcam
+    image_view: np.ndarray = field(default_factory=lambda: np.ones((480, 640, 4), dtype=np.float32))
 
-dpg.create_context()
+    def __enter__(self):
+        dpg.create_context()
 
-with dpg.texture_registry(show=False):
-    dpg.add_raw_texture(
-        width=640,
-        height=480,
-        default_value=image_view,
-        tag='texture-webcam',
-    )
+        with dpg.texture_registry(show=False):
+            dpg.add_raw_texture(
+                width=640,
+                height=480,
+                default_value=self.image_view,
+                tag='texture-webcam',
+            )
 
-with dpg.window(label='WebCam'):
-    dpg.add_image("texture-webcam")
+        with dpg.window(label='WebCam'):
+            dpg.add_image("texture-webcam")
 
-dpg.create_viewport(title='DearPyGuiCam', width=800, height=600)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-while dpg.is_dearpygui_running():
-    update_image()
-    dpg.render_dearpygui_frame()
+        dpg.create_viewport(title='DearPyGuiCam', width=800, height=600)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+        return self
+        
 
-dpg.destroy_context()
+    def __exit__(self, type, value, tb):
+        dpg.destroy_context()
+
+    def update_image(self, image) -> None:
+        view.image_view[:, :, :3] = image / 255
+
+    def run(self):
+        while dpg.is_dearpygui_running():
+            frame = self.webcam.get_frame()
+            view.update_image(image=frame)
+            dpg.render_dearpygui_frame()
+
+
+
+@dataclass
+class OpenCVWebcam:
+    cap: cv2.VideoCapture = field(default_factory=lambda: cv2.VideoCapture(0))
+
+    def get_frame(self) -> np.ndarray:
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (640, 480))
+            return frame
+   
+   
+with DearPyGuiView(webcam=OpenCVWebcam()) as view:
+    view.run()
+
+
     
