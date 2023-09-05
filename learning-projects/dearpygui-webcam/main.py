248d969ea3627a0f6281d28a7c0528a1ef9224f9
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-05 23:25:26 +0200

    R: minor

--- learning-projects/dearpygui-webcam/main.py
+++ learning-projects/dearpygui-webcam/main.py
@@ -1,16 +1,8 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-import numpy as np
-from dearpygui import dearpygui as dpg
-
 from gui import DPGController, DPGPresenter
 from webcam_opencv import OpenCVWebcam
 
-
-
-
-## Main
 
 controller = DPGController(
     webcam=OpenCVWebcam(),
