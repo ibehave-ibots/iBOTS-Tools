ca8cae194c7c9862740e041878889a952ecf1b27
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-11 17:29:49 +0200

    fixed behave tests to use presenter
    Co-authored-by: Mohammad Bashiri <mohammadbashiri93@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>
    Co-authored-by: bearums <19364641+bearums@users.noreply.github.com>

--- workshop-registration/features/environment.py
+++ workshop-registration/features/environment.py
@@ -1,12 +1,19 @@
+from unittest.mock import Mock
 from behave import fixture
-from app import AppModel, ListWorkshopsWorkflow
+
+from app.list_workshops_app import ListWorkshopsPresenter
+from app import ListWorkshopsWorkflow
 from adapters import InMemoryWorkshopRepo, InMemoryRegistrationRepo
 
 @fixture
 def before_scenario(context, scenario):
+    context.presenter = Mock(ListWorkshopsPresenter)
     workshop_repo = InMemoryWorkshopRepo()
     registration_repo = InMemoryRegistrationRepo()
-    app_model = AppModel()
     context.workshop_repo = workshop_repo
     context.registration_repo = registration_repo
-    context.app = ListWorkshopsWorkflow(workshop_repo=workshop_repo, registration_repo=registration_repo, model=app_model)
+    context.app = ListWorkshopsWorkflow(
+        workshop_repo=workshop_repo, 
+        registration_repo=registration_repo, 
+        presenter=context.presenter,
+    )
