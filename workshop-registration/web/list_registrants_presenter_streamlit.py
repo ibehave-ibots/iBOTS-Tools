1e155ac41a362b991e92d9403dca1c146e0f7e52
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-21 17:20:25 +0200

    added placeholder for data editor

--- workshop-registration/web/list_registrants_presenter_streamlit.py
+++ workshop-registration/web/list_registrants_presenter_streamlit.py
@@ -3,17 +3,49 @@ from app import ListRegistrantPresenter, RegistrantSummary
 import streamlit as st
 import pandas as pd
 
-class StreamlitRegistrantPresenter(ListRegistrantPresenter):
+class View:
 
     def __init__(self) -> None:
+        st.write("## Registrants")
+        self._table = st.empty()
+
+    def update_registrants_table(self, table: pd.DataFrame) -> None:
+        self._table.data_editor(
+            data=table,
+            # width=None,
+            # height=None,
+            # use_container_width=False,
+            # hide_index=None,
+            # column_order=None,
+            # column_config=dict(  # todo: add more specific rendering options
+            #     name=,
+            #     email: str
+            #     status: Literal['approved','waitlisted','rejected']
+            #     registered_on: str
+            #     group_name: str
+            #     workshop_id: str
+            #     id: str = field(default_factory=lambda: str(uuid4()))
+            # ),
+            # num_rows='fixed',
+            # key=None,
+            # on_change=None,
+            # args=None,
+            # kwargs=None
+        )
+
+
+
+class StreamlitRegistrantPresenter(ListRegistrantPresenter):
+
+    def __init__(self, view: View = View()) -> None:
         self.df = None
-        self.df_widget = st.empty()
+        self._view = view
 
     def show(self, registrants: List[RegistrantSummary]) -> None:
-        self.df = pd.DataFrame([registrant._asdict() for registrant in registrants])
+        self.df = pd.DataFrame([registrant.to_dict() for registrant in registrants])
         self.df.set_index("id", inplace=True)
-        self.df_widget.write(self.df) 
+        self._view.update_registrants_table(table=self.df)
     
     def show_update(self, registrant: RegistrantSummary) -> None:
-        self.df.loc[registrant.id] = pd.Series(registrant._asdict())
-        self.df_widget.write(self.df)
+        self.df.loc[registrant.id] = pd.Series(registrant.to_dict())
+        self._view.update_registrants_table(table=self.df)
