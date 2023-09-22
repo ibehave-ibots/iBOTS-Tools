ff04a305dd0d07c5b2d0f2fbd588611900b78008
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-22 09:09:29 +0200

    simpler script

--- workshop-registration/web/view.py
+++ workshop-registration/web/view.py
@@ -4,36 +4,37 @@ import streamlit as st
 
 @dataclass
 class Model:
-    table: pd.DataFrame = field(default_factory=lambda: pd.DataFrame(data=[], columns=['name', 'status']))
+    table: pd.DataFrame = field(default_factory=lambda: pd.DataFrame(data=[], columns=['name', 'status', 'state']))
+
+    def set_data(self, data):
+        self.table = pd.DataFrame(data, columns=['name', 'status', 'state'])
 
     def update_registrant_status(self, id, status):
         self.table.loc[id, 'status'] = status
+        self.table.loc[id, 'state'] = status
 
 if not 'model' in st.session_state:
-    st.session_state['model'] = Model(
-        table=pd.DataFrame([
-            {'name': 'Anna', 'status': 'waitlisted'},
-            {'name': 'Banana','status': 'waitlisted'},
-        ])
-    )
+    model = Model()
+    model.set_data([
+        {'name': 'Anna', 'status': 'waitlisted'},
+        {'name': 'Banana','status': 'waitlisted'},
+    ])
+    st.session_state['model'] = model
+    
 
 
 model: Model = st.session_state['model']
-
-st.data_editor(model.table, key="data_editor")
-editor_updates = st.session_state['data_editor']
-if editor_updates['edited_rows']:
-    for idx, changes in editor_updates['edited_rows'].items():
+def update():    
+    updated_rows = st.session_state['data_editor']['edited_rows']
+    assert len(updated_rows) == 1
+    for idx, changes in updated_rows.items():
         match idx, changes:
             case int(i), {"status": str(new_status)}:
-                ...
                 model.update_registrant_status(id=i, status=new_status)
             case _:
-                ...
-                # st.error("This change shouldn't be possible!")
-    st.experimental_rerun()
+                st.write("This change shouldn't be possible!", )
 
+st.data_editor(model.table, key="data_editor", on_change=update)
 
-st.write(model.table)
 
 
