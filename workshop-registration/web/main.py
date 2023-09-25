709fd1751be5b4cf7b52c7a0a1bbec6a9ee15e3c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-09-25 18:16:56 +0200

    remove create_repo()

--- workshop-registration/web/main.py
+++ workshop-registration/web/main.py
@@ -7,17 +7,44 @@ import streamlit as st
 from app.app import App
 from app import RegistrantWorkflows
 from web.presenter import Presenter
-from web.webapp import View
-from web.create_repo import create_reg_repo
+from web.view import View
+
+
+from adapters import InMemoryRegistrationRepo
+from app.registrationrepo import RegistrationRecord
 
 
 if 'initialized' not in st.session_state:
+
+    repo = InMemoryRegistrationRepo(
+        registrations=[
+            RegistrationRecord(
+                id="54321",
+                workshop_id="12345",
+                name="eve",
+                email="e@e.com",
+                registered_on="25092023",
+                custom_questions=[{'value': 'Prof. Sangee'}],
+                status='approved',
+            ),
+            RegistrationRecord(
+                id="11111",
+                workshop_id="12345",
+                name="adam",
+                email="a@a.com",
+                registered_on="26092023",
+                custom_questions=[{'value': 'Prof. Bee'}],
+                status='waitlisted',
+            ),
+        ],
+    )
+
     presenter = Presenter()
     view = View(
         controller=App(
             workshop_workflow=Mock(),
             registrant_workflows=RegistrantWorkflows(
-                registration_repo=create_reg_repo(),
+                registration_repo=repo,
                 presenter=presenter,
             )
         )
