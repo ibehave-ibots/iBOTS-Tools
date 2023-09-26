81cccc94e077f58f013234b5e2aa9c26b2c527a9
Ben Hastings <benhastings17@gmail.com>
2023-09-26 16:19:24 +0200

    refactoring mistakes fixed

--- workshop-registration/web/tests/test_presenter.py
+++ workshop-registration/web/tests/test_presenter.py
@@ -1,13 +1,11 @@
 from unittest.mock import Mock
-from web.view_model import ViewModel
+from web.view_model import AppState, ViewModel
 from web.presenter import Presenter
-from web.observable import Observable
 
 
 
 def test_show_sets_model_with_new_registrants():
-    state = Mock(Observable)
-    state.data = Mock(ViewModel)
+    state = AppState(data=Mock())
     new_model = Mock(ViewModel)
     state.data.update_registrant_table.return_value = new_model
     presenter = Presenter(state=state)
@@ -17,8 +15,7 @@ def test_show_sets_model_with_new_registrants():
     
 
 def test_show_update_sets_model_with_new_registrant():
-    state = Mock(Observable)
-    state.data = Mock(ViewModel)
+    state = AppState(data=Mock())
     new_model = Mock(ViewModel)
     state.data.set_registration_status.return_value = new_model
     presenter = Presenter(state=state)
