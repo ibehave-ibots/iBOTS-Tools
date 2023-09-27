2448f2754f8fc32a2e78a702c29859d0ac6e40b1
Sangeetha <an.sangeetha@gmail.com>
2023-09-27 10:28:36 +0200

    random registrant to a test meeting

--- workshop-registration/external/zoom_api/tests/test_add_delete_registrants.py
+++ workshop-registration/external/zoom_api/tests/test_add_delete_registrants.py
@@ -5,19 +5,19 @@ from external.zoom_api.list_registrants import list_registrants, ZoomRegistrant
 from pytest import mark
 import requests
 
-def test_registrants_added(access_token):
-    workshop_id = "824 9123 9311"
-    registrants_before_test = list_registrants(access_token=access_token, meeting_id=workshop_id, status="pending")
-    assert len(registrants_before_test) == 0
-     
-    add_random_registrant(access_token=access_token, meeting_id=workshop_id)
-    
-    registrants = list_registrants(access_token=access_token, meeting_id=workshop_id, status="pending")
+def test_registrants_added(access_token, setup_sandbox, meeting_id):
+    response = setup_sandbox
+
+    registrants = list_registrants(access_token=access_token, meeting_id=meeting_id, status="pending")
     assert len(registrants) > 0
 
+@pytest.fixture
+def meeting_id():
+    meeting_id = "824 9123 9311"
+    return meeting_id
   
-
-def add_random_registrant(access_token: str, meeting_id: str) -> None:
+@pytest.fixture
+def setup_sandbox(access_token: str, meeting_id: str) -> None:
     fname = 'test'+str(random.randint(1, 10))
     params = {
     "first_name": fname,
@@ -25,15 +25,15 @@ def add_random_registrant(access_token: str, meeting_id: str) -> None:
     "email": fname+'@lname.com',
     }
     response = requests.post(
-        url=f"https://api.zoom.us/v2//meetings/{meeting_id.replace(' ','')}/registrants",
+        url=f"https://api.zoom.us/v2/meetings/{meeting_id.replace(' ','')}/registrants",
         headers={"Authorization": f"Bearer {access_token}"},
         json=params,
         )
     response.raise_for_status()
     yield response
-    # registrant_id = response.json()['registrant_id']
-    # response = requests.delete(
-    #     url=f"https://api.zoom.us/v2/meetings/{meeting_id.replace(' ', '')}/registrants/{registrant_id}",
-    #     headers={"Authorization": f"Bearer {access_token}"},
-    #     )
-    # response.raise_for_status()
+    registrant_id = response.json()['registrant_id']
+    response = requests.delete(
+        url=f"https://api.zoom.us/v2/meetings/{meeting_id.replace(' ', '')}/registrants/{registrant_id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+        )
+    response.raise_for_status()
