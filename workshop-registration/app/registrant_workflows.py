5a3430bd0bfd5d59b5fc3ebbaa66f364b7e0f520
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-09-28 13:49:39 +0000

    Remove redundancy
    Co-authored-by: Ben Hastings <benhastings17@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- workshop-registration/app/registrant_workflows.py
+++ workshop-registration/app/registrant_workflows.py
@@ -28,7 +28,7 @@ class RegistrantWorkflows(NamedTuple):
             to_status: Literal['approved', 'waitlisted', 'rejected'],
         )  -> None:
         registrations = self.registration_repo.get_registrations(workshop_id=workshop_id)
-        for idx, registration in enumerate(registrations):
+        for registration in registrations:
             if registration.id == registration_id:
                 updated_registration = replace(registration, status=to_status)
                 self.registration_repo.update_registration(registration=updated_registration)
