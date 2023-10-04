de38ab0b27c6c638b027ae50b9396498f4fd495e
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-10-04 13:38:19 +0000

    Improve test
    Co-authored-by: Ben Hastings <benhastings17@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- workshop-registration/app/tests/test_change_decision.py
+++ workshop-registration/app/tests/test_change_decision.py
@@ -8,8 +8,16 @@ from app.app import App
 from adapters import InMemoryRegistrationRepo
 
 
-@mark.parametrize("new_status",[("rejected","waitlisted","approved")])
-def test_registration_decision_cannot_be_changed(new_status: Literal['approved','rejected', 'waitlisted']):
+@mark.parametrize(
+    ("status", "new_status"),
+    [
+    ("rejected", "approved"),
+    ("rejected", "waitlisted"),
+    ("approved", "rejected"),
+    ("approved", "waitlisted"),
+    ]
+    )
+def test_registration_decision_cannot_be_changed(status, new_status):
 
     #GIVEN
     #a registrant has a status that is NOT waitlisted
@@ -22,28 +30,25 @@ def test_registration_decision_cannot_be_changed(new_status: Literal['approved',
                 email="e@e.com",
                 registered_on="25092023",
                 custom_questions=[{'value': 'Prof. Sangee'}],
-                status='approved',
+                status=status,
                 )
             ],
     )
 
-    app= App(
-            workshop_workflow=Mock(),
-            registrant_workflows=RegistrantWorkflows(
+    workflow = RegistrantWorkflows(
                 registration_repo=repo,
-                presenter=Mock(),
-            )
-    )
+                presenter=Mock()
+                )
     #WHEN
     # the registrant's status is changed 
-    with patch.object(InMemoryRegistrationRepo, "update_registration" ):
+    with patch.object(InMemoryRegistrationRepo, "update_registration"):
         try:
-            app.update_registration_status(
-                                workshop_id= "12345", 
-                                registration_id= "54321",
-                                to_status= new_status)
-        except ZoomRegistrantStatusError as e:
+            workflow.update_registrant_status(
+                                workshop_id="12345", 
+                                registration_id="54321",
+                                to_status=new_status)
+        except:
             pass
         #THEN
         # the registrant's status is NOT updated
-        app.registrant_workflows.registration_repo.update_registration.assert_not_called()
+        workflow.registration_repo.update_registration.assert_not_called()
