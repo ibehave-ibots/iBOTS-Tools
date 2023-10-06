f3fa5dab91b2dfb8f26e22a0d0c30aac0659a8a3
Ben Hastings <benhastings17@gmail.com>
2023-10-06 12:33:39 +0200

    improved formatting

--- workshop-registration/adapters/attendance_summary_presenter_make_spreadsheet.py
+++ workshop-registration/adapters/attendance_summary_presenter_make_spreadsheet.py
@@ -30,8 +30,7 @@ class Spreadsheet_attendancepresenter(AttendancePresenter):
     def write_csv(self, attendance_summaries: List[AttendanceSummary], output_filename: str) -> None:
         
         string_IO = io.StringIO()
-        with redirect_stdout(string_IO):
-            self.show(attendance_summaries)
+        with redirect_stdout(string_IO): self.show(attendance_summaries)
         out = string_IO.getvalue()
 
         with open(output_filename, 'w') as f: f.write(out)
