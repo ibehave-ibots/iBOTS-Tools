9cb32e3a50963df9175881f1de48383ec1f6c2f2
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-10-09 09:08:51 +0000

    Add Zoom attendance endpoint
    Co-authored-by: Ben Hastings <benhastings17@gmail.com>
    Co-authored-by: Sangeetha <an.sangeetha@gmail.com>

--- workshop-registration/adapters/tests/test_attendancerepo_zoom.py
+++ workshop-registration/adapters/tests/test_attendancerepo_zoom.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+import os
 from unittest.mock import Mock
-from external.zoom_api import ZoomAttendee, OAuthGetToken
+import pytest
+
+from dotenv import load_dotenv
+from external.zoom_api import ZoomAttendee, OAuthGetToken, get_attendees
 from adapters import ZoomAttendanceRepo
 
 def test_zoom_attendance_repo_returns_correct_attendance_records_for_a_given_workshop():
@@ -8,7 +12,7 @@ def test_zoom_attendance_repo_returns_correct_attendance_records_for_a_given_wor
         ZoomAttendee(
             name="first_A last_A",
             user_email="a@a.com",
-            session="1",
+            session=1,
             join_time="2023-10-05T07:40:14Z",
             leave_time="2023-10-05T10:01:19Z",
         ),
@@ -16,7 +20,7 @@ def test_zoom_attendance_repo_returns_correct_attendance_records_for_a_given_wor
         ZoomAttendee(
             name="first_B last_B",
             user_email="b@b.com",
-            session="1",
+            session=1,
             join_time="2023-10-05T07:45:14Z",
             leave_time="2023-10-05T09:59:19Z",
         ),
@@ -35,3 +39,20 @@ def test_zoom_attendance_repo_returns_correct_attendance_records_for_a_given_wor
     assert attendance_records[0].session
     assert attendance_records[0].arrived == datetime.strptime("2023-10-05T07:40:14Z", "%Y-%m-%dT%H:%M:%S%z")
     assert attendance_records[0].departed == datetime.strptime("2023-10-05T10:01:19Z", "%Y-%m-%dT%H:%M:%S%z")
+    
+@pytest.mark.slow
+def test_zoom_attendance_repo_returns_correct_attendees_for_a_given_zoom_workshop():
+    load_dotenv()
+    oauth = OAuthGetToken(
+        client_id=os.environ["CLIENT_ID"],
+        client_secret=os.environ["CLIENT_SECRET"],
+        account_id=os.environ["ACCOUNT_ID"],
+    )
+
+    repo = ZoomAttendanceRepo(oauth_get_token=oauth, get_attendees=get_attendees)
+    workshop_id = "81504158493"
+
+    attendance_records = repo.get_attendance_records(workshop_id=workshop_id)
+    assert len(attendance_records) == 6
+    assert attendance_records[0].workshop_id == workshop_id
+    
