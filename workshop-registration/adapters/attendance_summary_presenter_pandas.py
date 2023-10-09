7a2755e29f29604e5cd5c49fb123d6b964212cb0
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2023-10-09 11:04:50 +0000

    Add write_csv method to the presenter

--- workshop-registration/adapters/attendance_summary_presenter_pandas.py
+++ workshop-registration/adapters/attendance_summary_presenter_pandas.py
@@ -5,8 +5,8 @@ from app import AttendancePresenter, AttendanceSummary
 
 
 class PandasAttendancePresenter(AttendancePresenter):
-
-    def show(self, attendance_summaries: List[AttendanceSummary]) -> None:
+    @staticmethod
+    def create_pandas_dataframe_from_attendance_summary(attendance_summaries: List[AttendanceSummary]) -> pd.DataFrame:
         unique_sessions = []
         for registrant in attendance_summaries:
             sessions = list(registrant.hours_per_session.keys())
@@ -19,4 +19,12 @@ class PandasAttendancePresenter(AttendancePresenter):
                 hours_per_session = registrant.hours_per_session.get(unique_session, 0.)
                 row[unique_session] = f"{hours_per_session:.2f}"
             df.loc[len(df)] = row
+        return df
+    
+    def show(self, attendance_summaries: List[AttendanceSummary]) -> None:
+        df = self.create_pandas_dataframe_from_attendance_summary(attendance_summaries)
         print(df.to_string())
+
+    def write_csv(self, attendance_summaries: List[AttendanceSummary], output_filename: str) -> None:
+        df = self.create_pandas_dataframe_from_attendance_summary(attendance_summaries)
+        df.to_csv(f"{output_filename}.csv")
