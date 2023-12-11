3301a7a0311271898044c14158e4e79f41ce6854
Ben Hastings <benhastings17@gmail.com>
2023-12-11 15:36:50 +0100

    attendance calc

--- workshop-registration/attendance.py
+++ workshop-registration/attendance.py
@@ -1,42 +1,61 @@
-
-
-# %%
 import os
-from external.zoom_api.zoom_oauth import OAuthGetToken
-from adapters.attendance_summary_presenter_make_spreadsheet import SpreadsheetAttendancePresenter
-from adapters.attendancerepo_zoom import ZoomAttendanceRepo
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
 from datetime import time, datetime
 from dataclasses import asdict
-import pandas as pd
+from external.zoom_api.zoom_oauth import OAuthGetToken
+from adapters.attendancerepo_zoom import ZoomAttendanceRepo
+from merge_timerange import merge_timeranges
 
 
-# %%
-meeting_id = "869 0642 6337"
 
-# %%
-oauth_get_token = OAuthGetToken(
-    client_id=os.environ["CLIENT_ID"],
-    client_secret=os.environ["CLIENT_SECRET"],
-    account_id=os.environ["ACCOUNT_ID"],
-)
-repo = ZoomAttendanceRepo(oauth_get_token=oauth_get_token)
-attendance_records = repo.get_attendance_records(workshop_id=meeting_id)
+def calculate_attendance(meeting_id):
+    load_dotenv()
+    oauth_get_token = OAuthGetToken(
+        client_id=os.environ["CLIENT_ID"],
+        client_secret=os.environ["CLIENT_SECRET"],
+        account_id=os.environ["ACCOUNT_ID"],
+    )
+    repo = ZoomAttendanceRepo(oauth_get_token=oauth_get_token)
+    attendance_records = repo.get_attendance_records(workshop_id=meeting_id)
 
-# %%
-data = [asdict(attendance_record) for attendance_record in attendance_records]
-df = pd.DataFrame(data=data)
+    data = [asdict(attendance_record) for attendance_record in attendance_records]
+    df = pd.DataFrame(data=data)
 
-# %%
-df['date'] = df.arrived.dt.date
-df['workshop_start'] = df.apply(lambda x: datetime.combine(x.date, time(hour= 8, minute=00)), axis = 1)
-df['workshop_end'] = df.apply(lambda x: datetime.combine(x.date, time(hour= 11, minute=30)), axis = 1)
+    df['date'] = df.arrived.dt.date
+    df['workshop_start'] = df.apply(lambda x: datetime.combine(x.date, time(hour= 8, minute=00)), axis = 1)
+    df['workshop_end'] = df.apply(lambda x: datetime.combine(x.date, time(hour= 11, minute=30)), axis = 1)
+
+    df["arrival_time_corrected"] = df["arrived"].dt.tz_localize(None).clip(lower=df["workshop_start"], upper=df["workshop_end"])
+    df["departure_time_corrected"] = df["departed"].dt.tz_localize(None).clip(lower=df["workshop_start"], upper=df["workshop_end"])
+
+    res_list = []
+    for n, df_n in df.groupby(["email", "date"]):
+        email = n[0]
+        workshop_day = n[1]
+        time_ranges_input = [(a,b) for a,b in zip(df_n["arrival_time_corrected"].values, df_n["departure_time_corrected"].values)]
+        merged_timeranges = merge_timeranges(time_ranges_input)
+        total_time_hrs = sum( [ ((mt[1]-mt[0]).astype(np.int64)*1e-9/3600) for mt in merged_timeranges ])
+        res_list.append({'email':email,'day':workshop_day, 'time_hrs':total_time_hrs })
 
 
-# %%
-df["arrival_time_corrected"] = df["arrived"].dt.tz_localize(None).clip(lower=df["workshop_start"], upper=df["workshop_end"])
-df["departure_time_corrected"] = df["departed"].dt.tz_localize(None).clip(lower=df["workshop_start"], upper=df["workshop_end"])
-df["time_spent_corrected"] = (df['departure_time_corrected'] - df['arrival_time_corrected']).dt.total_seconds()/3600
-df.groupby(["email", "date"]).time_spent_corrected.sum().unstack().round(1).style.background_gradient()
-# %%
-df[(df.email == "liyi3387@gmail.com") & (df.session == 'Day7')]
+    df_res = pd.DataFrame(res_list)
+    df_wide = df_res.pivot(index='email', columns='day', values='time_hrs').fillna(0.0).round(1).reset_index()
+    days = sorted(df_res.day.unique())
+    df_wide.rename(columns={ x: "Day%s"%(i+1) for x,i in zip(days, range(len(days)))})
+    return df_wide
 
+
+
+if __name__ == "__main__":
+    #"869 0642 6337" # '826 3181 8166'
+    # 
+
+    meeting_id = input("Type meeting id: ")
+
+    print("Calculating attendance for meeting id %s"%meeting_id)
+    df = calculate_attendance(meeting_id)
+    file_name = "attendance_report-%s.csv"%meeting_id.replace(" ","")
+    print('Saving to %s'%file_name)
+    df.to_csv(file_name , index=False)
