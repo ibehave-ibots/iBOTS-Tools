799241f1a388ab624bc21c928ca2d1923cf1d8d2
Ben Hastings <benhastings17@gmail.com>
2024-01-08 14:31:51 +0100

    variables exercises

--- workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
+++ workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
 
+const int redLED = 3;
+int greenLED = 4;
+int purpleLED = 5;
+int delay_time_s = 1;
+int delay_time_ms = delay_time_s*1000;
 void setup() {
-  pinMode(3, OUTPUT); // set pin 3 as an output
-  digitalWrite(3, HIGH); // turn pin 3 to high
+  pinMode(redLED, OUTPUT); 
+  pinMode(greenLED, OUTPUT);
+  pinMode(purpleLED, OUTPUT);
 }
 
 void loop() {
-
+  digitalWrite(redLED, HIGH); 
+  delay(delay_time_ms); 
+  digitalWrite(redLED, LOW); 
+  delay(delay_time_ms); 
+  redLED++;
 }
