bda2dd0fae0e2cbf035382af8f02c1a10ed74d66
Ben Hastings <benhastings17@gmail.com>
2024-01-09 17:23:30 +0100

    updates

--- workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
+++ workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
@@ -1,20 +1,51 @@
 #include <Arduino.h>
 
 const int redLED = 3;
-int greenLED = 4;
-int purpleLED = 5;
-int delay_time_s = 1;
-int delay_time_ms = delay_time_s*1000;
+const int greenLED = 4;
+const int purpleLED = 5;
+
+const int redBtn = 11;
+const int greenBtn = 10;
+const int purpleBtn = 9;
+
+bool greenLEDon = false;
+
 void setup() {
   pinMode(redLED, OUTPUT); 
   pinMode(greenLED, OUTPUT);
   pinMode(purpleLED, OUTPUT);
+
+  pinMode(redBtn, INPUT_PULLUP);
+  pinMode(greenBtn, INPUT_PULLUP);
+  pinMode(purpleBtn, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(redLED, HIGH); 
-  delay(delay_time_ms); 
-  digitalWrite(redLED, LOW); 
-  delay(delay_time_ms); 
-  redLED++;
-}
+ 
+  Serial.println('hello!');
+  delay(2000);
+
+  int greenBtnState = digitalRead(greenBtn);
+  // if (greenBtnState == LOW && greenLEDon == false ){
+  //   digitalWrite(greenLED, HIGH); 
+  //   greenLEDon = true;
+  //   }
+  // else if (greenBtnState == LOW && greenLEDon == true){
+  //   digitalWrite(greenLED, LOW); 
+  //   greenLEDon = false;
+  // }
+  delay(30);
+  if (greenBtnState == LOW){
+    if (greenLEDon == false){
+       digitalWrite(greenLED, HIGH); 
+       greenLEDon = true;
+    }
+    else {
+       digitalWrite(greenLED, LOW); 
+      greenLEDon = false;
+      delay(30);
+    }
+  }
+
+  }
