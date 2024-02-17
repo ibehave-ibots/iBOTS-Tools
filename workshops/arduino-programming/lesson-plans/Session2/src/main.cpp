3c9de70d2f361cf421a06a3e8423ecf6d8822d09
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-02-17 17:36:26 +0100

    sesssion 2

--- workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
+++ workshops/arduino-programming/lesson-plans/Session2/src/main.cpp
@@ -1,51 +1,11 @@
-#include <Arduino.h>
+// Development Plan
+// [ ] Write and build the boilerplate code for Arduino (#include<Arduino.h>, setup(), loop())
+// [ ] Open the simulator
+// [ ] Make the red LED flash on and off once every 50 milliseconds.
+// [ ] Make the red LED flash on and off only if one of the buttons is being pressed.
+// [ ] Turn on an LED when the button of a corresponding color is held down.
+// [ ] The blue LED should be different; it should turn off when its button is pressed, not on.
+// [ ] The green LED should be a switch; when its button is pressed, it should change from staying on to staying off, then go back the next time it's pressed.
+// [ ] When the red button is pressed, it should turn on for a couple of seconds, then automatically turn back off.
+// [ ] All buttons' LEDs should respond as soon as they are pressed.
 
-const int redLED = 3;
-const int greenLED = 4;
-const int purpleLED = 5;
-
-const int redBtn = 11;
-const int greenBtn = 10;
-const int purpleBtn = 9;
-
-bool greenLEDon = false;
-
-void setup() {
-  pinMode(redLED, OUTPUT); 
-  pinMode(greenLED, OUTPUT);
-  pinMode(purpleLED, OUTPUT);
-
-  pinMode(redBtn, INPUT_PULLUP);
-  pinMode(greenBtn, INPUT_PULLUP);
-  pinMode(purpleBtn, INPUT_PULLUP);
-  Serial.begin(115200);
-}
-
-void loop() {
- 
-  Serial.println('hello!');
-  delay(2000);
-
-  int greenBtnState = digitalRead(greenBtn);
-  // if (greenBtnState == LOW && greenLEDon == false ){
-  //   digitalWrite(greenLED, HIGH); 
-  //   greenLEDon = true;
-  //   }
-  // else if (greenBtnState == LOW && greenLEDon == true){
-  //   digitalWrite(greenLED, LOW); 
-  //   greenLEDon = false;
-  // }
-  delay(30);
-  if (greenBtnState == LOW){
-    if (greenLEDon == false){
-       digitalWrite(greenLED, HIGH); 
-       greenLEDon = true;
-    }
-    else {
-       digitalWrite(greenLED, LOW); 
-      greenLEDon = false;
-      delay(30);
-    }
-  }
-
-  }
