13882f8740bdf76b0a21e8c925f41691b5b2e9e4
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-02-17 17:07:08 +0100

    update session 1

--- workshops/arduino-programming/lesson-plans/Session1/src/main.cpp
+++ workshops/arduino-programming/lesson-plans/Session1/src/main.cpp
@@ -1,8 +1,21 @@
+// Development Plan:
+// [x] Showed up to the workshop.
+// [ ] This file was opened
+// [ ] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
+// [ ] The Red LED is turned on.
+// [ ] The Green LED is also turned on.
+// [ ] The Blue LED is also turned on.
+// [ ] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
+// [ ] The Red LED then turns on and off every second, forever.
+// [ ] The Green LED simultaneously turns on and off every half-second, forever.
+// [ ] The Blue LED is on whenever the Green LED is off, and vice-versa.
+
+
+
 #include <Arduino.h>
 
 void setup() {
-  pinMode(3, OUTPUT); // set pin 3 as an output
-  digitalWrite(3, HIGH); // turn pin 3 to high
+
 }
 
 void loop() {
