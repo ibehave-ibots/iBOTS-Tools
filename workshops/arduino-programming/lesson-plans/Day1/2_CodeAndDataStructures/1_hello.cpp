b6d680961b97820cd1ce8ceae53b4f5a1f01d9ae
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-02-17 20:28:20 +0100

    sript

---
+++ workshops/arduino-programming/lesson-plans/Day1/2_CodeAndDataStructures/1_hello.cpp
@@ -0,0 +1,13 @@
+// Directions:  Print
+
+// Reference:
+    // #include <iostream>
+    // int main(){}
+    // std::cout << "Hello, World!\n"
+    // g++ 1_hello.cpp -o 1_hello 
+    
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, World!\n";
+}
