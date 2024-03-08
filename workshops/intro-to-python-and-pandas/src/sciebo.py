e83d2b2d3a9a2a17e013bfcf0c40b9c31d059044
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2024-03-08 11:15:18 +0100

    R: strategy refactor of sciebo module

--- workshops/intro-to-python-and-pandas/src/sciebo.py
+++ workshops/intro-to-python-and-pandas/src/sciebo.py
@@ -1,19 +1,41 @@
 from pathlib import Path
 import requests
+from typing import Union
 from tqdm import tqdm
 
-def download_from_sciebo(public_url, to_filename, is_file = True):
-    """
-    Downloads a file or folder from a shared URL on Sciebo.
-    """
-    # Create the folder if a longer path was described
-    path = Path(to_filename)
-    if len(path.parts) > 1:
-        Path(to_filename).parent.mkdir(parents=True, exist_ok=True)
+def download_file(public_url, to_filename):
+    """Downloads a file from a shared URL on Sciebo."""
+    _make_parent_folders(to_filename)
 
     r = requests.get(public_url + "/download", stream=True)
-    progress_bar = tqdm(desc=f"Downloading {to_filename}", unit='B', unit_scale=True, total=int(r.headers['Content-Length'])) if is_file else tqdm()
+    num_bytes = int(r.headers['Content-Length'])
+    progress_bar = tqdm(desc=f"Downloading {to_filename}", unit='B', unit_scale=True, total=num_bytes)
     with open(to_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=8192):
             f.write(chunk)
             progress_bar.update(len(chunk))
+
+
+def download_folder(public_url, to_filename):
+    """Downloads a folder from a shared URL on Sciebo."""
+    _make_parent_folders(to_filename)
+    
+    r = requests.get(public_url + "/download", stream=True)
+    progress_bar = tqdm()
+    with open(to_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+            progress_bar.update(len(chunk))
+
+
+def download_from_sciebo(public_url, to_filename, is_file = True):
+    """Wrapper function: Downloads a file or folder from a shared URL on Sciebo."""
+    download_fun = download_file if is_file else download_folder
+    download_fun(public_url=public_url, to_filename=to_filename)
+
+
+def _make_parent_folders(path: Union[Path, str]) -> None:
+    # Create the parent folders if a longer path was described
+    path = Path(path)
+    if len(path.parts) > 1:
+        Path(to_filename).parent.mkdir(parents=True, exist_ok=True)
